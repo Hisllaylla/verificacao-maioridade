515b3439541435f7836b55c6a2e4ee2262feb0b4
Hisllaylla <hisllayllak@gmail.com>
2023-09-16 22:29:32 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace VerificacaoMaioridade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite sua idade: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int idade))
+            {
+                if (idade >= 18)
+                {
+                    Console.WriteLine("Você é maior de idade.");
+                }
+                else
+                {
+                    Console.WriteLine("Você é menor de idade.");
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Erro! Tente novamente.");
+            }
+        }
+    }
+}
